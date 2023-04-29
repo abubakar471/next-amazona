c1f8bfdef7c7f6eb911cd24cb1532d452e35cfbd
unknown <abubakar471>
2023-04-29 17:42:16 +0600

    tailwind css added

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next Amzaona - Home</title>
+        <meta name="description" content="next amazona online e-commerce platform" />
+      </Head>
+
+      <div>
+        <p className="text-3xl font-bold">Next Amazona</p>
+      </div>
+    </div>
+  )
+}
