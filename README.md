9720a54a2cef84f85a400597b786dd0bcbcf668a
unknown <abubakar471>
2023-04-30 14:46:52 +0600

    material ui and useStyles added

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-Amazon clone using next js 
+# Amazon clone using next js 
+
+
+# Error Notes:
+
+> when added the grid classname even after adding tailwind it didn't work
+    for the first time, so i simply remove all the className attribute
+    and wrote that attribute simply again and that time it worked.
+
+> when added useStyles hook for material ui it didnt work as it's import
+    format has been changed , we have to install @mui/styles first than 
+    have to import it from it like this,
+
+    yarn add @mui/styles
+
+    after installing this go to utils dir from this project folder and 
+    there import it like
+
+    import { makeStyles } from '@mui/styles';
+
+    now we can use it,
+
+    again it first time didn't work, so i removed all the code again and
+    simply written all the code just as before and it worked.
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,30 +4,31 @@ import React from 'react'
 import Container from '@mui/material/Container';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import useStyles from '../utils/styles'
 
 const Layout = ({ children, title }) => {
+    const classes = useStyles();
+
     return (
         <>
             <Head>
                 <title>{title ? title + ' -Next Amzona' : 'Next Amzona'}</title>
                 <meta name="description" content="next amazona online e-commerce platform" />
             </Head>
-            <div className='flex min-h-screen flex-col justify-between'>
 
-                <AppBar position="static">
-                    <Toolbar>
-                        <Link href="/" className="text-lg font-bold">
-                            amazona
-                        </Link>
-                    </Toolbar>
-                </AppBar>
-                <Container>
-                    {children}
-                </Container>
-                <footer className='flex items-center justify-center h-10 shadow-inner'>
-                    <p>Copyright &copy; 2023 | developed by abu bakar siddique </p>
-                </footer>
-            </div>
+            <AppBar position="static" className={classes.navbar}>
+                <Toolbar>
+                    <Link href="/" className="text-lg font-bold">
+                        amazona
+                    </Link>
+                </Toolbar>
+            </AppBar>
+            <Container className={classes.main}>
+                {children}
+            </Container>
+            <footer className={classes.footer}>
+                <p>Copyright &copy; 2023 | developed by abu bakar siddique </p>
+            </footer>
         </>
     )
 }
